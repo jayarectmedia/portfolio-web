e47fcea446949aa02aafabbbb9c5ae4f02659c6e
Jaya Saputra <jaya.saputra@talenavi.com>
2023-03-24 21:17:57 +0800

    feat:[MJ]Navbar Header

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins"', "sans-serif"],
+      },
+      colors: {
+        text: "#393F48",
+        brand: "#EF5DA8",
+      },
+    },
+  },
+  plugins: [],
+};
